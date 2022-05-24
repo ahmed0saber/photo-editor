1b4fd532264596d02664be764d7b4c2af47fc16d
Ahmed Saber <ahmed0saber33@gmail.com>
2022-05-24 18:37:00 +0200

    Finished

--- script.js
+++ script.js
@@ -7,21 +7,39 @@ window.onload = () => {
     foto = new Foto()
 }
 const makeGrayScale = () => {
+    if(!imageImported){
+        return
+    }
     foto.grayscale()
 }
 const makeBright = () => {
+    if(!imageImported){
+        return
+    }
     foto.makeBright()
 }
 const makeDark = () => {
+    if(!imageImported){
+        return
+    }
     foto.makeDark()
 }
 const makeBlur = () => {
+    if(!imageImported){
+        return
+    }
     foto.applyBlurFilter()
 }
 const makeEmboss = () => {
+    if(!imageImported){
+        return
+    }
     foto.applyEmbossFilter()
 }
 const makeSharp = () => {
+    if(!imageImported){
+        return
+    }
     foto.applySharpFilter()
 }
 var imageImported = false
